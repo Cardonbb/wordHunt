33a92669dd32537c11518d997d20ce979e78386b
Cardonbb <70238812+Cardonbb@users.noreply.github.com>
2025-04-06 22:11:46 -0700

    Add files via upload

---
+++ myOcr/model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+class TileCNN(nn.Module):
+    def __init__(self):
+        super(TileCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(8192, 128)
+        self.fc2 = nn.Linear(128, 24)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
